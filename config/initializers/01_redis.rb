afe28f48b6c48d6f375dc5fe43eb75d82b8f96fd
Danial Jenuil <dannyroy1002@gmail.com>
2021-02-03 15:27:50 +0800

    Update code to precompile assets for production container

--- config/initializers/01_redis.rb
+++ config/initializers/01_redis.rb
@@ -1,2 +1,5 @@
-$redis = Redis.new(url: Rails.application.secrets.redis, thread_safe: true)
-$redis.ping
+# no need to run if we are compiling assets
+if !defined?(::Rake::SprocketsTask)
+  $redis = Redis.new(url: Rails.application.secrets.redis, thread_safe: true)
+  $redis.ping
+end
