d7398718ec40071c47ac04976a6d5eaec7bb8e7e
Danial Jenuil <dannyroy1002@gmail.com>
2021-02-08 03:30:05 +0800

    Update Redis initializer and update Dockerfile.app.prod

--- config/initializers/01_redis.rb
+++ config/initializers/01_redis.rb
@@ -1,5 +1,5 @@
 # no need to run if we are compiling assets
-if !defined?(::Rake::SprocketsTask)
+if ENV['ASSETS_PRECOMPILE'].blank?
   $redis = Redis.new(url: Rails.application.secrets.redis, thread_safe: true)
   $redis.ping
 end
